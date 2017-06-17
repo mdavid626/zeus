b4b8e352e151dc6fee9b107e2c33405887caeb11
Dávid Molnár <xmolna02@stud.fit.vutbr.cz>
2017-06-17 10:09:38 +0200

    add sqltracker project

---
+++ src/Zeus.SqlTracker/SqlTracker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zeus.Common;
+
+namespace Zeus.Trackers
+{
+    public class SqlTracker : ITracker
+    {
+        public void Track(TrackedEvent trackedEvent)
+        {
+            
+        }
+
+        public IEnumerable<ProductStatistics> GetStatistics(DateTime maxDate)
+        {
+            return null;
+        }
+    }
+}
