917a052a032bcf5a7ca5b080d6586bffba5637ce
Dávid Molnár <xmolna02@stud.fit.vutbr.cz>
2017-06-17 11:07:48 +0200

    add unit tests for TrackController

--- src/Zeus.Web.Tests/TrackControllerTest.cs
+++ src/Zeus.Web.Tests/TrackControllerTest.cs
@@ -17,12 +17,61 @@ namespace Zeus.Web.Tests
             var ids = new[] {1, 2, 3};
             var type = TrackedEventType.List;
             var tracker = Substitute.For<ITracker>();
-
             var controller = new TrackController(tracker);
+
             var response = controller.Get(ids, type);
 
             tracker.Received().Track(Arg.Is<TrackedEvent>(t => t.Ids.SequenceEqual(ids) && t.Type == type));
             Assert.IsTrue(response is OkResult);
+        }
+
+        [TestMethod]
+        public void TestGetWithoutParams()
+        {
+            var tracker = Substitute.For<ITracker>();
+            var controller = new TrackController(tracker);
+
+            var response = controller.Get(null);
+
+            tracker.DidNotReceive().Track(Arg.Any<TrackedEvent>());
+            Assert.IsTrue(response is BadRequestResult);
+        }
+
+        [TestMethod]
+        public void TestGetWithoutIds()
+        {
+            var tracker = Substitute.For<ITracker>();
+            var controller = new TrackController(tracker);
+
+            var response = controller.Get(null, TrackedEventType.List);
+
+            tracker.DidNotReceive().Track(Arg.Any<TrackedEvent>());
+            Assert.IsTrue(response is BadRequestResult);
+        }
+
+        [TestMethod]
+        public void TestGetWithoutType()
+        {
+            var tracker = Substitute.For<ITracker>();
+            var controller = new TrackController(tracker);
+
+            var response = controller.Get(new [] {1, 2, 3});
+
+            tracker.DidNotReceive().Track(Arg.Any<TrackedEvent>());
+            Assert.IsTrue(response is BadRequestResult);
+        }
+
+        [TestMethod]
+        public void TestGetModelError()
+        {
+            var tracker = Substitute.For<ITracker>();
+            var controller = new TrackController(tracker);
+            controller.ModelState.AddModelError("ids", "not valid");
+
+            var response = controller.Get(new [] { 1, 2, 3 }, TrackedEventType.List);
+
+            tracker.DidNotReceive().Track(Arg.Any<TrackedEvent>());
+            Assert.IsTrue(response is BadRequestResult);
         }
     }
 }
