54a6ab175bf46d1b9f81e524fa634f38382c58d6
Dávid Molnár <xmolna02@stud.fit.vutbr.cz>
2017-06-17 11:53:23 +0200

    add unit tests for EnumModelBilder

--- src/Zeus.Web.Tests/EnumModelBinderTest.cs
+++ src/Zeus.Web.Tests/EnumModelBinderTest.cs
@@ -4,16 +4,102 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zeus.Web.Binders;
 
 namespace Zeus.Web.Tests
 {
     [TestClass]
-    public class EnumModelBinderTest
+    public class EnumModelBinderTest : ModelBinderTestBase
     {
-        [TestMethod]
-        public void TestMethod1()
+        private enum TestEnum
         {
-            
+            Item1,
+            Item2,
+            Item3
+        }
+
+        [TestMethod]
+        public void TestItem1()
+        {
+            var bindingContext = CreateBindingContext<TestEnum>("Item1");
+            var binder = new EnumModelBinder<TestEnum>();
+
+            var result = binder.BindModel(null, bindingContext);
+
+            Assert.IsTrue(result);
+            Assert.AreEqual(bindingContext.Model, TestEnum.Item1);
+        }
+
+        [TestMethod]
+        public void TestItem1Nullable()
+        {
+            var bindingContext = CreateBindingContext<TestEnum?>("Item1");
+            var binder = new EnumModelBinder<TestEnum>();
+
+            var result = binder.BindModel(null, bindingContext);
+
+            Assert.IsTrue(result);
+            Assert.AreEqual(bindingContext.Model, TestEnum.Item1);
+        }
+
+        [TestMethod]
+        public void TestItem1LowerCase()
+        {
+            var bindingContext = CreateBindingContext<TestEnum>("item2");
+            var binder = new EnumModelBinder<TestEnum>();
+
+            var result = binder.BindModel(null, bindingContext);
+
+            Assert.IsTrue(result);
+            Assert.AreEqual(bindingContext.Model, TestEnum.Item2);
+        }
+
+        [TestMethod]
+        public void TestItem1UpperCase()
+        {
+            var bindingContext = CreateBindingContext<TestEnum>("ITEM3");
+            var binder = new EnumModelBinder<TestEnum>();
+
+            var result = binder.BindModel(null, bindingContext);
+
+            Assert.IsTrue(result);
+            Assert.AreEqual(bindingContext.Model, TestEnum.Item3);
+        }
+
+        [TestMethod]
+        public void TestWrongItem()
+        {
+            var bindingContext = CreateBindingContext<TestEnum>("ItemX");
+            var binder = new EnumModelBinder<TestEnum>();
+
+            var result = binder.BindModel(null, bindingContext);
+
+            Assert.IsFalse(result);
+            Assert.IsNull(bindingContext.Model);
+        }
+
+        [TestMethod]
+        public void TestEmptyString()
+        {
+            var bindingContext = CreateBindingContext<TestEnum>("");
+            var binder = new EnumModelBinder<TestEnum>();
+
+            var result = binder.BindModel(null, bindingContext);
+
+            Assert.IsFalse(result);
+            Assert.IsNull(bindingContext.Model);
+        }
+
+        [TestMethod]
+        public void TestNullInput()
+        {
+            var bindingContext = CreateBindingContext<TestEnum>(null);
+            var binder = new EnumModelBinder<TestEnum>();
+
+            var result = binder.BindModel(null, bindingContext);
+
+            Assert.IsFalse(result);
+            Assert.IsNull(bindingContext.Model);
         }
     }
 }
---
+++ src/Zeus.Web.Tests/ModelBinderTestBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Metadata.Providers;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.ValueProviders;
+using NSubstitute;
+
+namespace Zeus.Web.Tests
+{
+    public class ModelBinderTestBase
+    {
+        protected ModelBindingContext CreateBindingContext<T>(string value, string modelName = "name")
+        {
+            var data = new DataAnnotationsModelMetadataProvider();
+            var modelMetadata = data.GetMetadataForType(null, typeof(T));
+            var valueProvider = Substitute.For<IValueProvider>();
+
+            if (value != null)
+            {
+                valueProvider
+                    .GetValue(Arg.Any<string>())
+                    .Returns(new ValueProviderResult(value, "", CultureInfo.InvariantCulture));
+            }
+            else
+            {
+                valueProvider.GetValue(Arg.Any<string>()).Returns(default(ValueProviderResult));
+            }
+
+            return new ModelBindingContext
+            {
+                ModelName = modelName,
+                ValueProvider = valueProvider,
+                ModelMetadata = modelMetadata
+            };
+        }
+    }
+}
