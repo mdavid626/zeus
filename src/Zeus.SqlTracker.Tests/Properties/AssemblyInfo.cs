6ae89f4802dfc9b9b62febabae981c2116156c31
Dávid Molnár <xmolna02@stud.fit.vutbr.cz>
2017-06-17 20:19:29 +0200

    add SqlTracker unit tests project

---
+++ src/Zeus.SqlTracker.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Zeus.SqlTracker.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Zeus.SqlTracker.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fd723d81-3a7e-4dac-87f9-110cfd64fc82")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Zeus.SqlTracker.Tests/SqlTrackerTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zeus.Trackers;
+
+namespace Zeus.SqlTrac2ker2.Tests
+{
+    [TestClass]
+    public class SqlTrackerTest
+    {
+        public const string ConnectionString = "Server=localhost;Database=zeus;Trusted_connection=True";
+
+        [TestMethod]
+        public void TestTrackEvent()
+        {
+            var tracker = new SqlTracker(ConnectionString);
+            tracker.Track(new TrackedEvent()
+            {
+                Ids = new [] {1, 2, 3},
+                Type = TrackedEventType.List,
+                CreationDate = DateTime.Now
+            });
+        }
+
+        [TestMethod]
+        public void TestGetStatistics()
+        {
+            var tracker = new SqlTracker(ConnectionString);
+            var stats = tracker.GetStatistics(DateTime.Now).ToList();
+        }
+    }
+}
