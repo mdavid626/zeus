7d46c359db4ec508c7d6e50cf7b53d0fd1d50247
Dávid Molnár <xmolna02@stud.fit.vutbr.cz>
2017-06-18 11:02:05 +0200

    extend unit tests for sql tracker

--- src/Zeus.SqlTracker.Tests/SqlTrackerTest.cs
+++ src/Zeus.SqlTracker.Tests/SqlTrackerTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
@@ -14,15 +16,47 @@ namespace Zeus.SqlTrac2ker2.Tests
         [TestMethod]
         public void TestTrackEvent()
         {
+            // arrange
             var connStringProvider = Substitute.For<IConnectionStringProvider>();
             connStringProvider.GetSqlConnectionString().Returns(s => ConnectionString);
             var tracker = new SqlTracker(connStringProvider);
+            var date = DateTime.Now;
+
+            // act
             tracker.Track(new TrackedEvent()
             {
                 Ids = new [] {1, 2, 3},
                 Type = TrackedEventType.List,
-                CreationDate = DateTime.Now
+                CreationDate = date
             });
+
+            // assert
+            var query = 
+                @"select count(*) as TrackedEventCount
+                  from TrackedEvent
+                  where ProductId = @productId and EventType = @eventType and EventDate = @eventDate";
+
+            using (var cn = new SqlConnection(ConnectionString))
+            {
+                cn.Open();
+                using (var cmd = new SqlCommand(query, cn))
+                {
+                    cmd.Parameters.Add("@productId", SqlDbType.Int).Value = 1;
+                    cmd.Parameters.Add("@eventType", SqlDbType.TinyInt).Value = TrackedEventType.List;
+                    cmd.Parameters.Add("@eventDate", SqlDbType.DateTime2).Value = date;
+
+                    var reader = cmd.ExecuteReader();
+
+                    var result = reader.Read();
+                    Assert.IsTrue(result);
+
+                    var count = (int)reader["TrackedEventCount"];
+                    Assert.IsTrue(count == 1);
+
+                    reader.Close();
+                }
+                cn.Close();
+            }
         }
 
         [TestMethod]
@@ -35,3 +69,4 @@ namespace Zeus.SqlTrac2ker2.Tests
         }
     }
 }
+
