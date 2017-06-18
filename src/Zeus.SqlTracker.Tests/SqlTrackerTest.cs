a32dc977883315465d2ecf91e7f7730f53c61245
Dávid Molnár <xmolna02@stud.fit.vutbr.cz>
2017-06-18 13:09:25 +0200

    add additional unit tests for sql tracker

--- src/Zeus.SqlTracker.Tests/SqlTrackerTest.cs
+++ src/Zeus.SqlTracker.Tests/SqlTrackerTest.cs
@@ -39,7 +39,7 @@ namespace Zeus.Trackers.Tests
         }
 
         [TestMethod]
-        public void TestGetStatistics()
+        public void TestGetStatisticsList()
         {
             // arrange
             var dbContextProvider = Substitute.For<ITrackedEventContextProvider>();
@@ -67,6 +67,104 @@ namespace Zeus.Trackers.Tests
                 Assert.IsTrue(item.DetailsViews == 0);
                 Assert.IsTrue(item.Conversions == 0);
                 Assert.IsTrue(item.ClickRate7Days == 0);
+                Assert.IsTrue(item.ConversionRate7Days == 0);
+                Assert.IsTrue(item.ConversionRate14Days == 0);
+            }
+        }
+
+        [TestMethod]
+        public void TestGetStatisticsClickRate()
+        {
+            // arrange
+            var dbContextProvider = Substitute.For<ITrackedEventContextProvider>();
+            using (var connection = Effort.DbConnectionFactory.CreateTransient())
+            {
+                dbContextProvider.Provide().Returns(p => new TrackedEventContext(connection, false));
+                var tracker = new SqlTracker(dbContextProvider);
+
+                var date = DateTime.Now.AddDays(-1);
+                var date4DaysBefore = DateTime.Now.AddDays(-4);
+
+                tracker.Track(new TrackedEvent()
+                {
+                    Ids = new[] { 1 },
+                    Type = TrackedEventType.List,
+                    CreationDate = date
+                });
+
+                tracker.Track(new TrackedEvent()
+                {
+                    Ids = new[] { 1 },
+                    Type = TrackedEventType.Details,
+                    CreationDate = date4DaysBefore
+                });
+
+                tracker.Track(new TrackedEvent()
+                {
+                    Ids = new[] { 1 },
+                    Type = TrackedEventType.Conversion,
+                    CreationDate = date4DaysBefore
+                });
+
+                // act
+                var stats = tracker.GetStatistics(DateTime.Now).ToList();
+                var item = stats.Single();
+
+                // assert
+                Assert.IsTrue(item.Id == 1);
+                Assert.IsTrue(item.ListImpressions == 1);
+                Assert.IsTrue(item.DetailsViews == 0);
+                Assert.IsTrue(item.Conversions == 0);
+                Assert.IsTrue(item.ClickRate7Days == 100);
+                Assert.IsTrue(item.ConversionRate7Days == 100);
+                Assert.IsTrue(item.ConversionRate14Days == 100);
+            }
+        }
+
+        [TestMethod]
+        public void TestGetStatisticsClickRate50Percent()
+        {
+            // arrange
+            var dbContextProvider = Substitute.For<ITrackedEventContextProvider>();
+            using (var connection = Effort.DbConnectionFactory.CreateTransient())
+            {
+                dbContextProvider.Provide().Returns(p => new TrackedEventContext(connection, false));
+                var tracker = new SqlTracker(dbContextProvider);
+
+                var date = DateTime.Now.AddDays(-1);
+                var date4DaysBefore = DateTime.Now.AddDays(-4);
+
+                tracker.Track(new TrackedEvent()
+                {
+                    Ids = new[] { 1 },
+                    Type = TrackedEventType.List,
+                    CreationDate = date
+                });
+
+                tracker.Track(new TrackedEvent()
+                {
+                    Ids = new[] { 1 },
+                    Type = TrackedEventType.List,
+                    CreationDate = date4DaysBefore
+                });
+
+                tracker.Track(new TrackedEvent()
+                {
+                    Ids = new[] { 1 },
+                    Type = TrackedEventType.Details,
+                    CreationDate = date4DaysBefore
+                });
+
+                // act
+                var stats = tracker.GetStatistics(DateTime.Now).ToList();
+                var item = stats.Single();
+
+                // assert
+                Assert.IsTrue(item.Id == 1);
+                Assert.IsTrue(item.ListImpressions == 1);
+                Assert.IsTrue(item.DetailsViews == 0);
+                Assert.IsTrue(item.Conversions == 0);
+                Assert.IsTrue(item.ClickRate7Days == 50);
                 Assert.IsTrue(item.ConversionRate7Days == 0);
                 Assert.IsTrue(item.ConversionRate14Days == 0);
             }
